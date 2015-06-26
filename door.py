351e3a0ab63ed1d4b8b7dd6bc67755708ead216c
mauro pizzamiglio <mauro.pizzamiglio@gmail.com>
2015-06-26 16:01:27 +0200

    Initial commit

---
+++ door.py
@@ -0,0 +1,39 @@
+from random import choice, randint
+
+__author__ = 'mauro'
+
+
+class Door:
+
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def getCoord(self):
+        return self.x, self.y
+
+    def setCoord(self, x, y):
+        self.x, self.y = x, y
+
+    def createDoor(self, building):
+        wall = choice(building.getEmptyWalls())
+        build_top_x, build_top_y = building.getTopCoord()
+        build_bottom_x, build_bottom_y = building.getBottomCoord()
+        if wall == 'n':
+            self.x = randint(build_top_x, build_bottom_x - 1)
+            self.y = build_top_y - 1
+            next_wall = 's'
+        elif wall == 's':
+            self.x = randint(build_top_x, build_bottom_x - 1)
+            self.y = build_bottom_y
+            next_wall = 'n'
+        elif wall == 'w':
+            self.x = build_top_x - 1
+            self.y = randint(build_top_y, build_bottom_y - 1)
+            next_wall = 'e'
+        elif wall == 'e':
+            self.x = build_bottom_x
+            self.y = randint(build_top_y, build_bottom_y - 1)
+            next_wall = 'w'
+
+        return self, next_wall
