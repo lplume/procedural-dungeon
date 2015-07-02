21e3fe73c064213965ae56714e44eb146fe6a98e
mauro pizzamiglio <mauro.pizzamiglio@gmail.com>
2015-07-02 13:12:41 +0200

    png map generation

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 Procedural python dungeon generator, based on http://vivid.gamedevs.org/creating-a-dungeon-generator-for-roquelikes/,
 at the link you can find an exhaustive description.
 
+Temporary Instructions
+===
+use python3
+install PIL
+"main": generator
+
+Credits
+===
+VividReality (http://vivid.gamedevs.org/) for the dungeon algorithm
+Clint Bellanger (http://opengameart.org/users/clint-bellanger) for the tiles
+
 Disclaimer
 ===
 Im sorry to hardcore pythonist for my bad syntax,
@@ -13,5 +24,4 @@ I push only the devel branch, because this is my very very first commit,
 i am gonna package sources better soon or later.
 
 My goal is to attach a tileset image, and output:
-* a png map
 * a tiled tmx file
--- dungeon.py
+++ dungeon.py
@@ -1,4 +1,4 @@
-import PIL
+from PIL import Image
 import configparser, pprint
 
 __author__ = 'mauro'
@@ -49,20 +49,91 @@ class Dungeon:
         self.buildings.append(building)
 
     def print(self):
-        pprint.pprint(self.config['DUNGEON']['tileset'])
-         # for d in self.doors:
-         #     door_x, door_y = d.getCoord()
-         #     self.dungeon[door_y][door_x] = 'D'
-         #
-         # for b in self.buildings:
-         #     x, y = b.getTopCoord()
-         #     w, h = b.getDimension()
-         #     for row in range(y, y + h):
-         #         for col in range(x, x + w):
-         #             self.dungeon[row][col] = ' '
-         #
-         # for row in range(0, self.rows):
-         #     for col in range(0, self.cols):
-         #         print(self.dungeon[row][col], end="")
-         #
-         #     print()
+        tileset = Image.open(self.config['DUNGEON']['tileset'])
+        tilesize = self.config.getint('DUNGEON', 'tilesize')
+
+        width, height = (abs(self.minx) + self.maxx + 1) * tilesize, (abs(self.miny) + self.maxy + 1) * tilesize
+        pprint.pprint(('immagine', width, height))
+
+        output = Image.new("RGB", (width, height), 'red')
+
+        empty_box = self.config['DUNGEON']['empty'].split(',')
+        tlx, tly = int(empty_box[0]) * tilesize, int(empty_box[1]) * tilesize
+        brx, bry = tlx + tilesize, tly + tilesize
+        empty = tileset.crop((tlx, tly, brx, bry))
+
+        wall_box = self.config['DUNGEON']['wall'].split(',')
+        tlx, tly = int(wall_box[0]) * tilesize, int(wall_box[1]) * tilesize
+        brx, bry = tlx + tilesize, tly + tilesize
+        wall = tileset.crop((tlx, tly, brx, bry))
+
+        door_box = self.config['DUNGEON']['door'].split(',')
+        tlx, tly = int(door_box[0]) * tilesize, int(door_box[1]) * tilesize
+        brx, bry = tlx + tilesize, tly + tilesize
+        door = tileset.crop((tlx, tly, brx, bry))
+
+        floor_box = self.config['DUNGEON']['floor'].split(',')
+        tlx, tly = int(floor_box[0]) * tilesize, int(floor_box[1]) * tilesize
+        brx, bry = tlx + tilesize, tly + tilesize
+        floor = tileset.crop((tlx, tly, brx, bry))
+
+        # empty flood
+        for i in range(0, width, tilesize):
+            for j in range(0, height, tilesize):
+                output.paste(empty, (i, j))
+
+
+        for b in self.buildings:
+            #width, height = b.getDimension()
+            tlx, tly, brx, bry = b.getCoord()
+            ttlx = tilesize * (tlx + abs(self.minx) - 1)
+            tbrx = tilesize * (brx + abs(self.minx))
+            ttly = tilesize * (tly + abs(self.miny) - 1)
+            tbry = tilesize * (bry + abs(self.miny))
+            pprint.pprint(('building top/bottom y', ttly, tbry))
+            for x in range(ttlx, tbrx, tilesize):
+                output.paste(wall, (x, ttly))
+                output.paste(wall, (x, tbry))
+            for y in range(ttly, tbry + 1, tilesize):
+                output.paste(wall, (ttlx, y))
+                output.paste(wall, (tbrx, y))
+
+        # doors
+        for d in self.doors:
+            x, y = d.getCoord()
+            tx = tilesize * (x + abs(self.minx))
+            ty = tilesize * (y + abs(self.miny))
+            pprint.pprint(('dor x, y', tx, ty))
+            output.paste(door, (tx, ty))
+
+        for b in self.buildings:
+            #width, height = b.getDimension()
+            tlx, tly, brx, bry = b.getCoord()
+            ttlx = tilesize * (tlx + abs(self.minx))
+            tbrx = tilesize * (brx + abs(self.minx))
+            ttly = tilesize * (tly + abs(self.miny))
+            tbry = tilesize * (bry + abs(self.miny))
+            pprint.pprint(('building top/bottom y', ttly, tbry))
+            for x in range(ttlx, tbrx, tilesize):
+                for y in range(ttly, tbry, tilesize):
+                    output.paste(floor, (x, y))
+
+
+        output.save('output.png')
+
+        for d in self.doors:
+         door_x, door_y = d.getCoord()
+         self.dungeon[door_y][door_x] = 'D'
+
+        for b in self.buildings:
+         x, y = b.getTopCoord()
+         w, h = b.getDimension()
+         for row in range(y, y + h):
+             for col in range(x, x + w):
+                 self.dungeon[row][col] = ' '
+
+        for row in range(0, self.rows):
+         for col in range(0, self.cols):
+             print(self.dungeon[row][col], end="")
+
+         print()
